f68d4b26bb76883ed16eecb88616bb997a8702f9
Victor Feng <nzvictor.feng@gmail.com>
2016-11-21 21:08:55 +1300

    initial commit.

---
+++ src/test/java/com/mycompany/app/NewTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
